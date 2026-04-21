c73775ed073f0da5cf73dcd04e88fb677a8d3e50
Abhinav Bangar <a.banger2005@gmail.com>
2026-04-21 23:12:14 +0530

    Added White Knights Moves

---
+++ README.md
@@ -0,0 +1,15 @@
+### BitBoard representation 
+```bash
+8 | . . . . . . . .
+7 | . . . . . . . .
+6 | . . . . . . . .
+5 | . . . . . . . .
+4 | . . . . . . . .
+3 | . . . . . . . .
+2 | . . . . . . . .
+1 | . . . . . . . .
+    - - - - - - - - 
+    h g f e d c b a
+```
+
+The board is flipped about the Positive y axis making it a little confusing for visualizing left and right of a piece .
--- movegen.cpp
+++ movegen.cpp
@@ -4,23 +4,45 @@
 using U64 = uint64_t ;
 using namespace std ;
 
-const U64 aFile = 0x7f7f7f7f7f7f7f7fULL ;
-const U64 hFile = 0xfefefefefefefefeULL ;
+const U64 notAFile = 0x7f7f7f7f7f7f7f7fULL ;
+const U64 notHFile = 0xfefefefefefefefeULL ;
+const U64 notABFile = 0xfcfcfcfcfcfcfcfcULL;
+const U64 notGHFile = 0x3f3f3f3f3f3f3f3fULL;
 const U64 whitePawnInitial = 0x000000000000FF00ULL ;
 const U64 blackPawnInitial = 0x00FF000000000000ULL ;
 
 U64 generateWhitePawnMoves(){
     U64 singlePush = (WP << 8) & ~allOcc ;
-    U64 doublePush = ((singlePush & whitePawnInitial) << 8) & ~allOcc ;
-    U64 leftCapture = (WP << 9) & aFile & blackOcc ;
-    U64 rightCapture = (WP << 7) & hFile & blackOcc ;
+    U64 doublePush = ((singlePush & whitePawnInitial) << 16) & ~allOcc ;
+    U64 leftCapture = (WP << 7) & notAFile & blackOcc ;
+    U64 rightCapture = (WP << 9) & notHFile & blackOcc ;
     return singlePush | doublePush |leftCapture | rightCapture;
 }
 
 U64 generateBlackPawnMoves(){
     U64 singlePush = (BP >> 8) & ~allOcc ;
     U64 doublePush = ((singlePush & blackPawnInitial) << 8) & ~allOcc ;
-    U64 leftCapture = (BP >> 9) & aFile & whiteOcc ;
-    U64 rightCapture = (BP >> 7) & hFile & whiteOcc ;
+    U64 leftCapture = (BP >> 9) & notAFile & whiteOcc ;
+    U64 rightCapture = (BP >> 7) & notHFile & whiteOcc ;
     return singlePush | doublePush |leftCapture | rightCapture;
 }
+
+U64 generateWhiteKnightMoves(){
+    U64 knightMoves = 0ULL;
+    knightMoves |= (WN & notGHFile ) << 6 ;
+    knightMoves |= (WN & notABFile ) << 10 ;
+    knightMoves |= (WN & notAFile ) << 15 ;
+    knightMoves |= (WN & notHFile ) << 17 ;
+
+    knightMoves |= (WN & notGHFile ) >> 10 ;
+    knightMoves |= (WN & notABFile ) >> 6 ;
+    knightMoves |= (WN & notAFile ) >> 17 ;
+    knightMoves |= (WN & notHFile ) >> 15 ;
+    knightMoves = knightMoves & ~whiteOcc ; //allowing to capture the blacks (hehe :))   
+}
+
+U64 generateWhiteBishopMoves(){
+
+}
+
+U64 generateWhiteRookMoves(){}

882a99e4aca1379d8ded84726de3ca4b686a43f2
Abhinav Bangar <a.banger2005@gmail.com>
2026-04-29 11:04:18 +0530

    Fixed Pawn Moves : Final

---
+++ src/movegen.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdint>
+
+using U64 = uint64_t ;
+
+U64 generateWhitePawnMoves() ;
+U64 generateBlackPawnMoves() ;
+U64 generateWhiteKnightMoves() ;
+U64 generateWhiteBishopMoves() ;
+U64 generateWhiteRookMoves();
